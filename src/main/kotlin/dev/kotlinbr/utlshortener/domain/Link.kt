76d5fa202719e3d3af14356d1e63c31d69753097
Lucas Fugisawa <lucasfugisawa@gmail.com>
2025-09-26 14:00:59 -0300

    Set default values for `id` and `createdAt` in `Link` model

--- src/main/kotlin/dev/kotlinbr/utlshortener/domain/Link.kt
+++ src/main/kotlin/dev/kotlinbr/utlshortener/domain/Link.kt
@@ -3,10 +3,10 @@ package dev.kotlinbr.utlshortener.domain
 import java.time.OffsetDateTime
 
 data class Link(
-    val id: Long?,
+    val id: Long? = null,
     val slug: String,
     val targetUrl: String,
-    val createdAt: OffsetDateTime,
+    val createdAt: OffsetDateTime = OffsetDateTime.now(),
     val isActive: Boolean = true,
     val expiresAt: OffsetDateTime? = null,
 )
